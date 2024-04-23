0b78318adb2f1e4cbd2be6ebcc0b5278bad5f1b5
Imanmachukwu Chukwuemeka <iamimanma@gmail.com>
2024-04-23 23:11:09 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,74 @@
+const leaveAnimation = (container) => {
+    const card = container.querySelector(".card");
+
+    function getOffset(el) {
+        const rect = el.getBoundingClientRect();
+        return {
+          left: rect.left + window.scrollX,
+          top: rect.top + window.scrollY
+        };
+    };
+    const xPosition = getOffset(card).left;
+    console.log(xPosition)
+    const yPosition = getOffset(card).top;
+    console.log(yPosition)
+
+    const tl = gsap.timeline({
+        defaults: {
+            ease: "expo.inOut",
+            duration: 2,
+        }
+    });
+
+    tl.to(container, {
+        height: "auto",
+    })
+
+    return tl;
+}
+
+const enterAnimation = (container) => {
+    const cardImage = container.querySelector(".card__image");
+    const card = container.querySelector(".card");
+
+    const tl = gsap.timeline({
+        defaults: {
+            ease: "expo.inOut",
+            duration: 2,
+        }
+    });
+
+    tl.to(cardImage, {
+        scale: 1,
+        objectPosition: "top"
+    }, 0).to(card, {
+        width: "100%"
+    }, 0).to(container, {
+        height: "auto"
+    }, 0)
+
+    return tl;
+}
+
+barba.init({
+    transitions: [{
+      name: 'basic-transition',
+      sync: true,
+      from: {
+        namespace: [
+          'home'
+        ]
+      },
+      to: {
+        namespace: [
+          'project'
+        ]
+      },
+      leave({current}) {
+        return leaveAnimation(current.container);
+      },
+      enter({next}) {
+        return enterAnimation(next.container);
+      }
+    }]
+  });
